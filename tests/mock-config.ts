18a48f5531155c8e2d9ecf871bab372dd326aa49
Yuan Gao <wind.memory.cn@gmail.com>
2018-07-09 12:39:21 +0800

    add integration test to verify change

---
+++ tests/mock-config.ts
@@ -0,0 +1,27 @@
+export const MOCK_USER_ID = 'fancy_boy'
+
+export const MOCK_TOKEN = 'int-mock-token'
+
+export const MOCK_SELF_CONTACT = {
+  // TODO - put padchat logo here
+  big_head: "padchat-logo-url", 
+  city: "Haidian", 
+  country: "CN", 
+  intro: "", 
+  label: "1", 
+  message: "", 
+  nick_name: "Padchat", 
+  provincia: "Beijing", 
+  py_initial: "P", 
+  quan_pin: "padchat", 
+  remark: "little_padpad", 
+  remark_py_initial: "little_padpad", 
+  remark_quan_pin: "LPP", 
+  sex: 1, 
+  signature: "I am a fancy boy", 
+  small_head: "padchat-small-logo-url", 
+  status: 0, 
+  stranger: "v1_f2dfd4342ed6415a0a1d59bf581aa9b5511c7688212c41c79acb3fd14a1e1526@stranger", 
+  ticket: "", 
+  user_name: MOCK_USER_ID
+}
