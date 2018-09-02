wechaty/wechaty-puppet-padchat

commit fee0e662361dc090b82d78049d8427654d12b4aa

suppress check scan status -2 warning

src/padchat-manager.ts
@@ -457,6 +457,10 @@ export class PadchatManager extends PadchatRpc {
             waitUserResponse = false
             break
 
+          case WXCheckQRCodeStatus.Ignore:
+            log.silly('PuppetPadchatManager', 'ignore status -2')
+            break
+
           default:
             log.warn('PuppetPadchatManager', 'startCheckScan() unknown WXCheckQRCodeStatus: ' + result.status)
             this.loginScanQrcode = undefined

src/padchat-rpc.type.ts
@@ -32,6 +32,7 @@ export interface WXGetQRCodeType {
 }
 
 export enum WXCheckQRCodeStatus {
+  Ignore      = -2,
   Unknown     = -1,
   WaitScan    = 0,
   WaitConfirm = 1,

commit a46848531b9362f5cfcc6f97e22359fa016c814c

fix bug introduced by fee0e662361dc090b82d78049d8427654d12b4aa

src/padchat-manager.ts
@@ -294,6 +294,8 @@ export class PadchatManager extends PadchatRpc {
     this.userId          = undefined
     this.loginScanQrcode = undefined
     this.loginScanStatus = undefined
+    this.contactListSynced = false
+    this.roomNeedsToBeSync = 0
 
     this.state.off(true)
   }
@@ -459,6 +461,9 @@ export class PadchatManager extends PadchatRpc {
 
           case WXCheckQRCodeStatus.Ignore:
             log.silly('PuppetPadchatManager', 'ignore status -2')
+            this.loginScanQrcode = undefined
+            this.loginScanStatus = undefined
+            waitUserResponse = false
             break
 
           default: